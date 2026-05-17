5639f2409acc2e3d74fbbae669bd6d82648012df
Jaroslav <jaroslav.morhul@gmail.com>
2026-05-17 20:10:09 +0200

    Add files via upload

---
+++ Task4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Socket serverSocket = new Socket(AddressFamily.InterNetwork,
+    SocketType.Dgram,
+    ProtocolType.Udp);
+
+IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 5005);
+serverSocket.Bind(endPoint);
+
+Console.WriteLine("UDP server is started on port 5005...");
+
+byte[] buffer = new byte[1024];
+
+while (true)
+{
+    EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+    
+    int receivedBytes = serverSocket.ReceiveFrom(buffer, ref remoteEndPoint);
+
+    string message = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+
+    Console.WriteLine($"Received from {remoteEndPoint}: {message}");
+    
+    string response = "Server receive: " + message;
+    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+    serverSocket.SendTo(responseBytes, remoteEndPoint);
+}
---
+++ Task5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+socket.ReceiveTimeout = 2000;
+
+IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Loopback, 5005);
+
+while (true)
+{
+    Console.Write("Enter message: ");
+    string message = Console.ReadLine();
+
+    byte[] data = Encoding.UTF8.GetBytes(message);
+    socket.SendTo(data, serverEndPoint);
+
+    try
+    {
+        byte[] buffer = new byte[1024];
+        EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+        int bytes = socket.ReceiveFrom(buffer, ref remoteEndPoint);
+        string response = Encoding.UTF8.GetString(buffer, 0, bytes);
+
+        Console.WriteLine("Server response: " + response);
+    }
+    catch (SocketException)
+    {
+        Console.WriteLine("Timeout: no response from server (2s)");
+    }
+}
